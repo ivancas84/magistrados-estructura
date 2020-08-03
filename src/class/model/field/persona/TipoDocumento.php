07cf10d58316f854be7fb97e481eca506713dd5e
ivancas84 <ivancas84@gmail.com>
2020-08-03 15:34:57 -0300

    Configurar opcion subtype = select

---
+++ src/class/model/field/persona/TipoDocumento.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("class/model/field/persona/_TipoDocumento.php");
+
+class FieldPersonaTipoDocumento extends _FieldPersonaTipoDocumento {
+
+  public $subtype = "select";
+  
+
+}
