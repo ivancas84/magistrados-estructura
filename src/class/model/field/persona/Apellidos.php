f4916088f8086a22a37a6b37093bf2a1ca0273e4
ivancas84 <ivancas84@gmail.com>
2020-08-06 10:20:38 -0300

    Configurar campos principales de persona

---
+++ src/class/model/field/persona/Apellidos.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("class/model/field/persona/_Apellidos.php");
+
+class FieldPersonaApellidos extends _FieldPersonaApellidos {
+
+  public $main = true;
+  
+
+}
---
+++ src/class/model/field/persona/Nombres.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("class/model/field/persona/_Nombres.php");
+
+class FieldPersonaNombres extends _FieldPersonaNombres {
+
+  public $main = true;
+  
+
+}
