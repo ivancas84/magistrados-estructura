ffc9105bec10ce212f74322003b7ae98e5859613
ivancas84 <ivancas84@gmail.com>
2020-08-18 00:12:27 -0300

    Incorporar valores por defecto

--- src/class/model/field/afiliacion/Estado.php
+++ src/class/model/field/afiliacion/Estado.php
@@ -4,6 +4,7 @@ require_once("class/model/field/afiliacion/_Estado.php");
 
 class FieldAfiliacionEstado extends _FieldAfiliacionEstado {
 
+  public $default = "Creado";
   public $subtype ="select_text";
   public $selectValues = ["Creado", "Enviado", "Aprobado", "Rechazado"];
 
