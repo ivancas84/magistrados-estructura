6cb3c64a8771769117bd9fbc835172743c8816bd
unknown <ivancas84@gmail.com>
2021-01-25 12:50:53 -0300

    Definir campos unique multiple para viatico

---
+++ src/class/model/entity/Viatico.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("class/model/entity/_Viatico.php");
+
+class ViaticoEntity extends _ViaticoEntity {
+  public $uniqueMultiple = ['periodo','organo','departamento_judicial'];
+
+
+}
