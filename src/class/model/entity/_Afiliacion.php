abdb043811e1007e5d6a3cd9ab331ac245440b13
ivancas84 <ivancas84@gmail.com>
2021-07-18 16:03:23 -0300

    Incorporar nuevo campo afiliacion.codigo

--- src/class/model/entity/_Afiliacion.php
+++ src/class/model/entity/_Afiliacion.php
@@ -6,7 +6,7 @@ require_once("class/model/Field.php");
 class _AfiliacionEntity extends Entity {
   public $name = "afiliacion";
   public $alias = "afil";
-  public $nf = ['motivo', 'estado', 'creado', 'enviado', 'evaluado', 'modificado', 'observaciones'];
+  public $nf = ['motivo', 'estado', 'creado', 'enviado', 'evaluado', 'modificado', 'observaciones', 'codigo'];
   public $mu = ['persona'];
   public $_u = [];
   public $notNull = ['id', 'motivo', 'estado', 'creado', 'persona'];
