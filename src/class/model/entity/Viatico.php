1bde9bf239e23736ed98cf22c08ba649c02b10d5
ivancas84 <ivancas84@gmail.com>
2022-05-13 13:43:44 -0300

    Quite el formato de uniqueMultiple porque me da error al generar

--- src/class/model/entity/Viatico.php
+++ src/class/model/entity/Viatico.php
@@ -3,7 +3,7 @@
 require_once("class/model/entity/_Viatico.php");
 
 class ViaticoEntity extends _ViaticoEntity {
-  public $uniqueMultiple = ['periodo.ym','departamento_judicial'];
+  public $uniqueMultiple = ['periodo','departamento_judicial'];
 
 
 }
